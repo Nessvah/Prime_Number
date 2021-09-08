0b667c57f5a35ca9ed404c9e1592fa4d464bfa4f
Nessvah <vanessamarquesns@outlook.pt>
2021-09-08 10:26:34 +0100

    restart

--- Prime_Number.c
+++ Prime_Number.c
@@ -1,7 +1,38 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main ()
 {
-    printf("First commit.\n");
+    //Hardcode prime numbers
+    int primes[100] = {[0] = 2, [1] = 3};
+   
+    int c, t;
+
+    for (int i = 0; i <= 100; i++)
+    {
+        c = 0;
+        for (int j = 2; j < primes[i]; j++)
+        {
+            if (primes[i] % j == 0)
+            {
+                c = 1;
+                break;
+            }            
+        }
+        if (c == 0)
+        {
+           primes[i]++;
+        }  
+    }
+    
+    for (int i = 0; i < primes[i]; i++)
+    {
+        printf("%d ", primes[i]);
+    }
+    
+    
+    
+    
+
     return 0;
 }
